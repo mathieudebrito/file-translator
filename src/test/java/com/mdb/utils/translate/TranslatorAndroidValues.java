ffc67576bc3ab9a7bcf14d91307eb5e2fd68359c
Mathieu De Brito <mathieu.debrito@gmail.com>
2015-01-06 16:53:56 +0100

    Added google play store tests Added google play store test resources

--- src/test/java/com/mdb/utils/translate/TranslatorAndroidValues.java
+++ src/test/java/com/mdb/utils/translate/TranslatorAndroidValues.java
@@ -1,5 +1,8 @@
 package com.mdb.utils.translate;
 
+import com.mdb.utils.translate.generators.android.ResourcesStringsFileGenerator;
+import com.mdb.utils.translate.parsers.android.ResourcesStringsFileParser;
+
 import java.util.ArrayList;
 
 public class TranslatorAndroidValues {
@@ -16,7 +19,7 @@ public class TranslatorAndroidValues {
         config.languageFrom = Language.ENGLISH;
         config.languageTo = Language.toList(Language.FRENCH);
         config.files = new ArrayList<FileToTranslate>();
-        config.files.add(new FileToTranslate(root, new FileParserAndroidStrings(), new FileGeneratorAndroidStrings()));
+        config.files.add(new FileToTranslate(root, new ResourcesStringsFileParser(), new ResourcesStringsFileGenerator()));
 
         Translator translator = new TranslatorGoogle();
         translator.init(config);
