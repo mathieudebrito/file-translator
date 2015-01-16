01bc03f1227bcc61d7b39de0ed9306dd5565a6a2
Mathieu De Brito <mathieu.debrito@gmail.com>
2015-01-16 12:12:29 +0100

    Update TranslatorAndroidValues.java

--- src/test/java/com/github/mathieudebrito/translator/TranslatorAndroidValues.java
+++ src/test/java/com/github/mathieudebrito/translator/TranslatorAndroidValues.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class TranslatorAndroidValues {
 
-    public static final String GOOGLE_API_KEY = "AIzaSyBKLWxBb2nMr9bIb4-PRUkHH7Df8-fxmAw";
+    public static final String GOOGLE_API_KEY = "";
 
     @org.junit.Test
     public void testInit() throws Exception {
